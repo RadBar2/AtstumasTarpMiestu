cb935d9f04c86bcb77bff102d855ef712c4759a6
Rapolas Kizis <rapolas.kizis@mif.stud.vu.lt>
2026-05-19 16:48:01 +0300

    Updated the header file

--- graph.c
+++ graph.c
@@ -1,1 +1,18 @@
+#include "graph.h"
+#include <stdio.h>
 
+weightedAdjacencyMatrix* createMatrix(int matrixSize) {
+
+}
+
+void visualize(weightedAdjacencyMatrix* matrix) {
+
+}
+
+int dijkstra(weightedAdjacencyMatrix* matrix, int sourceMember, int destinationMember) {
+
+}
+
+void reconstructPath(int* path, int sourceMember, int destinationMember) {
+
+}
--- graph.h
+++ graph.h
@@ -1,5 +1,14 @@
 #ifndef GRAPH_H_20260519
 #define GRAPH_H_20260519
 
+typedef struct {
+    int matrixSize;
+    int** weight;
+} weightedAdjacencyMatrix;
+
+weightedAdjacencyMatrix* createMatrix(int matrixSize);
+void visualize(weightedAdjacencyMatrix* matrix);
+int dijkstra(weightedAdjacencyMatrix* matrix, int sourceMember, int destinationMember);
+void reconstructPath(int* path, int sourceMember, int destinationMember);
 
 #endif // GRAPH_H_20260519
