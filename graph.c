ae25c75b4b32bd55679224ffae4517f4dd5fcfc9
Rapolas Kizis <rapolas.kizis@mif.stud.vu.lt>
2026-05-19 19:37:27 +0300

    Implemented dijkstra()

--- graph.c
+++ graph.c
@@ -1,14 +1,28 @@
 #include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 weightedAdjacencyMatrix* createMatrix(int matrixSize) {
     weightedAdjacencyMatrix *matrix = (weightedAdjacencyMatrix*)malloc(sizeof(weightedAdjacencyMatrix));
+    if (matrix == NULL) {
+        printf("Error: memory allocation failed.\n");
+        return NULL;
+    }
+
     matrix->matrixSize = matrixSize;
 
     matrix->weight = (int**)malloc(matrixSize * sizeof(int*));
+    if (matrix->weight == NULL) {
+        printf("Error: memory allocation failed.\n");
+        return NULL;
+    }
     for (int i = 0; i < matrixSize; ++i) {
         matrix->weight[i] = (int*)malloc(matrixSize * sizeof(int));
+        if (matrix->weight[i] == NULL) {
+        printf("Error: memory allocation failed.\n");
+        return NULL;
+    }
         for (int j = 0; j < matrixSize; ++j) {
             if (i != j)
                 matrix->weight[i][j] = -1;
@@ -60,6 +74,49 @@ int dijkstra(weightedAdjacencyMatrix* matrix, int sourceMember, int destinationM
         printf("Error: pointer cannot be NULL.\n");
         return -1;
     }
+
+    int distances[matrix->matrixSize];
+    bool visited[matrix->matrixSize];
+    for (int i = 0; i < matrix->matrixSize; ++i) {
+        if (i != sourceMember)
+            distances[i] = -1;
+        else
+            distances[i] = 0;
+
+        visited[i] = 0;
+        path[i] = -1; // Possible error here, if the capacity of path[] array is smaller than the matrixSize
+    }
+
+    for (int i = 0; i < matrix->matrixSize; ++i) {
+        int minDistance = -1;
+        int minIndex = -1;
+
+        for (int j = 0; j < matrix->matrixSize; ++j) {
+            if (distances[j] != -1 && !visited[j]) {
+                if (minDistance == -1 || distances[j] < minDistance) {
+                    minDistance = distances[j];
+                    minIndex = j;
+                }
+            }
+        }
+
+        if (minIndex == -1)
+            return -1;
+
+        if (minIndex == destinationMember)
+            return distances[minIndex];
+
+        visited[minIndex] = 1;
+        for (int j = 0; j < matrix->matrixSize; ++j) {
+            if (matrix->weight[minIndex][j] != -1 && !visited[j]
+                    && (distances[j] > distances[minIndex] + matrix->weight[minIndex][j] || distances[j] == -1)) {
+                distances[j] = distances[minIndex] + matrix->weight[minIndex][j];
+                path[j] = minIndex;
+            }
+        }
+    }
+
+    return -1;
 }
 
 void reconstructPath(int* path, int sourceMember, int destinationMember) {
@@ -68,10 +125,16 @@ void reconstructPath(int* path, int sourceMember, int destinationMember) {
         return;
     }
 
-    if (sourceMember == destinationMember)
+    if (sourceMember == destinationMember) {
         printf("%d", sourceMember);
-    else {
-        reconstructPath(path, sourceMember, path[destinationMember]);
-        printf(" -> %d", destinationMember);
+        return;
     }
+
+    if (path[destinationMember] == -1) {
+        printf("Path does not exist.");
+        return;
+    }
+
+    reconstructPath(path, sourceMember, path[destinationMember]);
+    printf(" -> %d", destinationMember);
 }
--- graph.h
+++ graph.h
@@ -9,7 +9,7 @@ typedef struct {
 weightedAdjacencyMatrix* createMatrix(int matrixSize);
 void deleteMatrix(weightedAdjacencyMatrix* matrix);
 void visualize(weightedAdjacencyMatrix* matrix);
-int dijkstra(weightedAdjacencyMatrix* matrix, int sourceMember, int destinationMember, int* path);
-void reconstructPath(int* path, int sourceMember, int destinationMember);
+int dijkstra(weightedAdjacencyMatrix* matrix, int sourceMember, int destinationMember, int* path); // path[] array must be allocated with the matrix->matrixSize capacity
+void reconstructPath(int* path, int sourceMember, int destinationMember); // it is unadvised to call reconstructPath() function when dijkstra() returns -1 (in other words, the path does not exist)
 
 #endif // GRAPH_H_20260519
